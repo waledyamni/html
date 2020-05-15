f40cc15d262368ebb198a515f9a967390e35a1d8
waledyamni <63209461+waledyamni@users.noreply.github.com>
2020-05-15 20:37:04 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+ function code(){
+     var codes = document.getElementById('codes').value;
+     document.getElementById('output').innerHTML = codes;
+
+ }
+function code2(){
+   
+    document.getElementById('output').innerHTML = "";
+
+}
